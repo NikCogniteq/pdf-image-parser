efab4b7c67fd1bf55e9beaf0dbeb17d0538a5d15
Nikita S <nikita.s@cogniteq.com>
2023-08-11 12:12:55 +0300

    init commit

---
+++ PdfImageParser/Program.cs
@@ -0,0 +1,29 @@
+﻿using Tesseract;
+
+namespace PdfImageParser
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var imageName = "Testbrev1.png"; // image from 'images' folder
+            var language = "swe"; // swe - Swedish, eng - English
+
+            var projectPath = Path.GetFullPath(@"..\..\..\");
+            var dataPath = Path.Combine(projectPath, "tessdata");
+            var imagePath = Path.Combine(projectPath, "images", imageName);
+
+            // Create a TesseractEngine
+            using var engine = new TesseractEngine(dataPath, language, EngineMode.Default);
+            // Load the image
+            using var img = Pix.LoadFromFile(imagePath);
+            // Perform OCR on the image
+            using var page = engine.Process(img);
+            // Get the recognized text
+            string text = page.GetText();
+            Console.WriteLine("Recognized Text:");
+            Console.WriteLine(text);
+            Console.ReadLine();
+        }
+    }
+}
